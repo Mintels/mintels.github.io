17de22077701692ef53e6f10b876cd234b3664f1
Evan Nicholas <evancninja@gmail.com>
2025-12-19 15:43:53 +0100

    Animation Changes
    Added Animations/Translations/Transforms to elements on the hero.

--- evannicholas.dev/src/Cards.jsx
+++ evannicholas.dev/src/Cards.jsx
@@ -1,11 +1,12 @@
 import styles from './Cards.module.css';
-
+import leftArrow from './assets/leftArrow.svg';
+import rightArrow from './assets/rightArrow.svg';
 export default function Cards() {
     return (
         <div className={styles.cardsContainer}>
-            <img src="https://via.placeholder.com/150" alt="Card 1" />
+            <img src={leftArrow} className={styles.arrow} alt="Right Arrow" />
             <p> In Progress </p>
-            <img src="https://via.placeholder.com/150" alt="Card 2" />
+            <img src={rightArrow} className={styles.arrow} alt="Left Arrow" />
         </div>
 
     )
